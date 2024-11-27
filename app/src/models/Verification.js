c57f11cda061458c8bd961d87b5c8ed95f825d56
shafiisawesome <shafi.shaik@informatik.hs-fulda.de>
2024-11-27 18:45:24 +0100

    Delete app folder from the repository as it is moved to frontend folder

--- app/src/models/Verification.js
+++
@@ -1,9 +0,0 @@
-class Verification {
-  constructor(verification_id, user_id, status, created_at, updated_at) {
-    this.verification_id = verification_id;
-    this.user_id = user_id;
-    this.status = status;
-    this.created_at = created_at;
-    this.updated_at = updated_at;
-  }
-}
