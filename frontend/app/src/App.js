362cdb0749c5105d3e9f34e401b60ef1d14f10f0
Devansh Negi <devanshnegi1@gmail.com>
2024-12-05 16:06:25 +0100

    Implemented LanlordHomepage,CreateNewListing,MyListing,EditListing in frontend. Version1.0

--- frontend/app/src/App.js
+++ frontend/app/src/App.js
@@ -2,6 +2,12 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import SearchResults from "./pages/SearchResults";
+import LandlordHomepage from "./pages/landlord/LandlordHomepage";
+import CreateNewListing from "./pages/landlord/CreateNewListing";
+import MyListing from "./pages/landlord/MyListing";
+import EditDeleteListing from "./pages/landlord/EditDeleteListing";
+
+
 
 const App = () => {
   return (
@@ -9,6 +15,16 @@ const App = () => {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/searchresults" element={<SearchResults />} />
+
+        <Route path="/landlord" element={<LandlordHomepage />} />
+        <Route path="/landlord/create-listing" element={<CreateNewListing />} />
+        <Route path="/landlord/my-listings" element={<MyListing />} />
+        <Route path="/landlord/edit-listing/:id" element={<EditDeleteListing />} />
+
+
+
+
+
       </Routes>
     </Router>
   );
