e0caaa621aa5bab9c9c5094db209afd29507578c
Muhammad Hassan <muhammad.hassan@informatik.hs-fulda.de>
2024-12-19 18:40:21 +0100

    update socket conn

--- backend/src/socket/socketConnection.js
+++ backend/src/socket/socketConnection.js
@@ -13,63 +13,66 @@ export const initiateSocket = (server) => {
       cors: {
         // origin: config.CLIENT_URL,
         origin: "*",
+        methods: ["GET", "POST"],
       },
     });
-    io.of("/wsapp").use((socket, next) => {
-      authenticateSocket(socket, next);
-    }).on("connection", (socket) => {
-      const userName = socket.decoded?.userName;
-      if (userName) {
-        socket.join(userName);
-        console.log(`User ${userName} joined room ${userName}`);
-      }
-      console.log(`A client has connected : ${socket.id}`);
-
-      socket.on("createConversation", async (data) => {
-        // console.log("======> data from client", data);
-
-        const conversation = await createConversation(socket, data);
-
-        // console.log("conversationn ==>", conversation);
-        if (conversation) {
-          socket.join(conversation.conversation.conversation_id);
+    io.of("/wsapp")
+      .use((socket, next) => {
+        authenticateSocket(socket, next);
+      })
+      .on("connection", (socket) => {
+        const userName = socket.decoded?.userName;
+        if (userName) {
+          socket.join(userName);
+          console.log(`User ${userName} joined room ${userName}`);
         }
-        socket.emit("createConversation", conversation);
-      });
+        console.log(`A client has connected : ${socket.id}`);
 
-      socket.on("sendMessage", async (data) => {
-        if (data) {
-          await createChat(data);
-          io.emit("sendMessage", {
-            message: data.message,
-            sender_id: data.sender_id,
-            createdAt: data.created_at,
-            conversation_id: data.conversation_id,
-          });
-          io.emit("updatedLastMessage", data.message);
-        }
-      });
+        socket.on("createConversation", async (data) => {
+          // console.log("======> data from client", data);
 
-      // Fetch all conversations for a user
-      socket.on("getConversations", async () => {
-        const conversations = await getConversations(socket);
+          const conversation = await createConversation(socket, data);
 
-        // Emit the fetched conversations back to the client
-        socket.emit("getConversations", conversations);
-      });
+          // console.log("conversationn ==>", conversation);
+          if (conversation) {
+            socket.join(conversation.conversation.conversation_id);
+          }
+          socket.emit("createConversation", conversation);
+        });
 
-      // Fetch all chats in a conversation
-      socket.on("getChats", async (data) => {
-        const chats = await getChats(socket, data);
-        console.log(socket.decoded, "decoded");
-        // Emit the fetched chats back to the client
-        io.to(socket.decoded.userName).emit("getChats", chats);
-      });
+        socket.on("sendMessage", async (data) => {
+          if (data) {
+            await createChat(data);
+            io.emit("sendMessage", {
+              message: data.message,
+              sender_id: data.sender_id,
+              createdAt: data.created_at,
+              conversation_id: data.conversation_id,
+            });
+            io.emit("updatedLastMessage", data.message);
+          }
+        });
 
-      socket.on("disconnect", () => {
-        console.log(`A client has disconnected : ${socket.id}`);
+        // Fetch all conversations for a user
+        socket.on("getConversations", async () => {
+          const conversations = await getConversations(socket);
+
+          // Emit the fetched conversations back to the client
+          socket.emit("getConversations", conversations);
+        });
+
+        // Fetch all chats in a conversation
+        socket.on("getChats", async (data) => {
+          const chats = await getChats(socket, data);
+          console.log(socket.decoded, "decoded");
+          // Emit the fetched chats back to the client
+          io.to(socket.decoded.userName).emit("getChats", chats);
+        });
+
+        socket.on("disconnect", () => {
+          console.log(`A client has disconnected : ${socket.id}`);
+        });
       });
-    });
   } catch (error) {
     console.log(error.message);
   }
